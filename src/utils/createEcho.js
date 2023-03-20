cf91015077148191d9a1b92b5c4e82f1bea4ac1c
Georgiy Malahov <87697829+borodos@users.noreply.github.com>
2023-03-20 17:29:08 +0300

    fix errors

--- src/utils/createEcho.js
+++ src/utils/createEcho.js
@@ -3,10 +3,11 @@ import { PUSHER_APP_CLUSTER, PUSHER_APP_AUTH_ENDPOINT } from "../constants";
 import Pusher from "pusher-js";
 import Echo from "laravel-echo";
 import connectToBroadcast from "./connectToBroadcast";
-export let VUE_ECHO = null;
+
+export let VUE_ECHO;
 
 export default (accessToken, userId) => {
-  if (window.Echo === undefined) {
+  if (VUE_ECHO === undefined) {
     VUE_ECHO = new Echo({
       authEndpoint: PUSHER_APP_AUTH_ENDPOINT,
       pusher: Pusher,
